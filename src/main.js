c58af28f675d9353692f3cb5faf1760a8603f16d
lijun <714810395@qq.com>
2018-03-30 18:24:37 +0800

    框架搭建

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Vuex from 'vuex'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(axios)
+Vue.use(ElementUI)
+// 引入mockjs
+require('./mock.js')
+/* eslint-disable ,no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
